b75123f4c8d9a6095255781b04a0afb4fdd062aa
UmG18 <umang18oct@gmail.com>
2017-01-30 00:16:40 +0530

    Basic Contact Layout added

--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,10 @@ function init_map(){
   };
   map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);
   marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(12.969264,79.15593799999999)});
-  google.maps.event.addListener(marker, 'click', function(){infowindow.open(map,marker);});
-  infowindow.open(map,marker);
+  // infowindow = new google.maps.InfoWindow({
+  //          content: ''
+  //      });
+  // google.maps.event.addListener(marker, 'click', function(){infowindow.open(map,marker);});
+  // infowindow.open(map,marker);
 }
 google.maps.event.addDomListener(window, 'load', init_map);
