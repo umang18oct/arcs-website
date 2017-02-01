dab906bf527ab4666b1f730156c9e0ab16934431
UmG18 <umang18oct@gmail.com>
2017-02-01 05:52:45 +0530

    Few changes

--- public/js/main.js
+++ public/js/main.js
@@ -31,3 +31,35 @@ function init_map(){
   // infowindow.open(map,marker);
 }
 google.maps.event.addDomListener(window, 'load', init_map);
+
+// $("#workshopNav").click(function() {
+//     $('html, body').animate({
+//         scrollTop: $("#workshops").offset().top
+//     }, 1500);
+// });
+//     var $animation_elements = $('.ws');
+//     var $window = $(window);
+//
+//     function check_if_in_view() {
+//       var window_height = $window.height();
+//       var window_top_position = $window.scrollTop();
+//       var window_bottom_position = (window_top_position + window_height);
+//
+//       $.each($animation_elements, function() {
+//         var $element = $(this);
+//         var element_height = $element.outerHeight();
+//         var element_top_position = $element.offset().top;
+//         var element_bottom_position = (element_top_position + element_height);
+//
+//         //check to see if this current container is within viewport
+//         if ((element_bottom_position >= window_top_position) &&
+//         (element_top_position <= window_bottom_position)) {
+//           $element.addClass('animated fadeInDown');
+//         } else {
+//           $element.removeClass('animated fadeInDown');
+//     }
+//   });
+// }
+//
+// $window.on('scroll resize', check_if_in_view);
+// $window.trigger('scroll');
