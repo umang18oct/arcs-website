a847edbdc6f39ac4d03bdd5bbf63cad2203bd463
UmG18 <umang18oct@gmail.com>
2017-02-14 23:09:45 +0530

    Some fixes made

--- public/js/main.js
+++ public/js/main.js
@@ -7,6 +7,73 @@ function rollingLoader() {
   document.getElementById("myDiv").style.display = "block";
 }
 */
+function stackChange(){
+if (window.innerWidth < 768){
+  document.getElementById("bg4").classList.remove("four")
+  document.getElementById("bg4").classList.remove("x");
+  document.getElementById("contactLarge").classList.remove("six");
+  document.getElementById("contactLarge").classList.remove("x");
+  document.getElementById("workshops").classList.remove("five");
+  document.getElementById("workshops").classList.remove("x");
+
+  document.getElementById("section1").classList.add("one");
+  document.getElementById("section1").classList.add("x");
+  document.getElementById("section2").classList.add("two");
+  document.getElementById("section2").classList.add("x");
+  document.getElementById("carousalMain").classList.add("three");
+  document.getElementById("carousalMain").classList.add("x");
+  document.getElementById("img3").classList.add("four");
+  document.getElementById("img3").classList.add("x");
+  document.getElementById("aboutSmall").classList.add("five");
+  document.getElementById("aboutSmall").classList.add("x");
+  document.getElementById("workshopHeading").classList.add("six");
+  document.getElementById("workshopHeading").classList.add("x");
+  document.getElementById("ws1").classList.add("seven");
+  document.getElementById("ws1").classList.add("x");
+  document.getElementById("ws2").classList.add("eight");
+  document.getElementById("ws2").classList.add("x");
+  document.getElementById("ws3").classList.add("nine");
+  document.getElementById("ws3").classList.add("x");
+  document.getElementById("map").classList.add("ten");
+  document.getElementById("map").classList.add("x");
+  document.getElementById("contact").classList.add("eleven");
+  document.getElementById("contact").classList.add("x");
+}
+else {
+  document.getElementById("img3").classList.remove("four")
+  document.getElementById("img3").classList.remove("x");
+  document.getElementById("aboutSmall").classList.remove("five")
+  document.getElementById("aboutSmall").classList.remove("x");
+  document.getElementById("workshopHeading").classList.remove("six");
+  document.getElementById("workshopHeading").classList.remove("x");
+  document.getElementById("ws1").classList.remove("seven");
+  document.getElementById("ws1").classList.remove("x");
+  document.getElementById("ws2").classList.remove("eight");
+  document.getElementById("ws2").classList.remove("x");
+  document.getElementById("ws3").classList.remove("nine");
+  document.getElementById("ws3").classList.remove("x");
+  document.getElementById("map").classList.remove("ten");
+  document.getElementById("map").classList.remove("x");
+  document.getElementById("contact").classList.remove("eleven");
+  document.getElementById("contact").classList.remove("x");
+
+  document.getElementById("section1").classList.add("one");
+  document.getElementById("section1").classList.add("x");
+  document.getElementById("section2").classList.add("two");
+  document.getElementById("section2").classList.add("x");
+  document.getElementById("carousalMain").classList.add("three");
+  document.getElementById("carousalMain").classList.add("x");
+  document.getElementById("bg4").classList.add("four")
+  document.getElementById("bg4").classList.add("x");
+  document.getElementById("workshops").classList.add("five");
+  document.getElementById("workshops").classList.add("x");
+  // $("sponsorship").classList.add("four x");
+  document.getElementById("contactLarge").classList.add("six");
+  document.getElementById("contactLarge").classList.add("x");
+}
+$('.x').stack();
+}
+
 function init_map(){
   var myOptions = {
     zoom:15,
